e05cefbc1091804a78e411b772fc07e0301c8a56
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-05-14 10:52:22 +0100

    Added proxy to api

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
+import axios from "axios";
+import { useEffect } from "react";
 function App() {
+	useEffect(() => {
+		axios
+			.get("/scrap")
+			.then(({ data }) => console.log(data))
+			.catch((err) => console.log(err));
+	}, []);
 	return (
 		<div className="App">
 			<h1>Mistho Frontend</h1>
