5b576edf19271b62e7d24b35b74884266f536d0a
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-05-14 13:47:23 +0100

    Working on fetching content

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,13 +4,25 @@ import { createStore, thunk, action, persist } from "easy-peasy";
 export default createStore(
 	persist({
 		token: null,
+		contents: [],
 		login: thunk(async (actions, payload) => {
 			const { data } = await axios.post("/auth/login", payload);
 			actions.setToken(data.token);
 			return data;
 		}),
+		getContents: thunk(async (actions, payload, { getState }) => {
+			const { token } = getState();
+			const { data } = await axios.get("/scrap", {
+				headers: { Authorization: `Bearer ${token}` },
+			});
+			actions.setContents(data.data);
+			return data;
+		}),
 		setToken: action((state, token) => {
 			state.token = token;
+		}),
+		setContents: action((state, payload) => {
+			state.contents = payload;
 		}),
 	})
 );
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,8 +1,29 @@
 import { Redirect } from "react-router-dom";
-import { useStoreState } from "easy-peasy";
+import { useStoreState, useStoreActions } from "easy-peasy";
+import { useEffect, useState } from "react";
+
 const Home = () => {
-	const easy_peasy = JSON.parse(localStorage.getItem("[EasyPeasyStore][0]"));
-	const token = useStoreState((state) => state.token) || easy_peasy.data.token;
+	const [loading, setLoading] = useState(false);
+	const token = useStoreState((state) => state.token);
+	const contents = useStoreState((state) => state.contents);
+
+	const getContents = useStoreActions((actions) => actions.getContents);
+
+	useEffect(() => {
+		const fetchContents = async () => {
+			setLoading(true);
+			try {
+				await getContents();
+			} catch (err) {
+				console.log(err);
+			}
+			setLoading(false);
+		};
+		if (token) {
+			fetchContents();
+		}
+	}, [token, getContents]);
+
 	if (!token) {
 		return (
 			<Redirect
@@ -15,6 +36,7 @@ const Home = () => {
 	return (
 		<div>
 			<h1>Home</h1>
+			{loading && <h1>Loading...</h1>}
 		</div>
 	);
 };
