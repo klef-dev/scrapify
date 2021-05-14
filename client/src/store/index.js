db05fb1168f7e84c12d7854a041beeb436fde295
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-05-14 13:09:50 +0100

    Worked on token bug

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,16 @@
 import axios from "axios";
-import { createStore, thunk, action } from "easy-peasy";
+import { createStore, thunk, action, persist } from "easy-peasy";
 
-export default createStore({
-	token: null,
-	login: thunk(async (actions, payload) => {
-		const { data } = await axios.post("/auth/login", payload);
-		actions.setToken(data.token);
-		return data;
-	}),
-	setToken: action((state, token) => {
-		state.token = token;
-	}),
-});
+export default createStore(
+	persist({
+		token: null,
+		login: thunk(async (actions, payload) => {
+			const { data } = await axios.post("/auth/login", payload);
+			actions.setToken(data.token);
+			return data;
+		}),
+		setToken: action((state, token) => {
+			state.token = token;
+		}),
+	})
+);
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,4 +1,17 @@
+import { Redirect } from "react-router-dom";
+import { useStoreState } from "easy-peasy";
 const Home = () => {
+	const easy_peasy = JSON.parse(localStorage.getItem("[EasyPeasyStore][0]"));
+	const token = useStoreState((state) => state.token) || easy_peasy.data.token;
+	if (!token) {
+		return (
+			<Redirect
+				to={{
+					pathname: "/login",
+				}}
+			/>
+		);
+	}
 	return (
 		<div>
 			<h1>Home</h1>
